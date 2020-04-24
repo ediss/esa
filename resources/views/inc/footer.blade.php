f05442a46c5868b234936849c8afc196f0a2aba0
Edis Skenderi <eskenderi@egamingc.com>
2020-04-24 12:30:18 +0200

    change address

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -120,7 +120,7 @@
                     <!-- Links -->
                     <h6 class="text-uppercase font-weight-bold">Contact</h6>
                     <hr class="deep-green accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-                    <p><i class="fas fa-home mr-3"></i> <br/>Valletta Buildings, 4th Floor, Suite 20, South Street, Valetta, VLT 1103, Malta</p>
+                    <p><i class="fas fa-home mr-3"></i> <br/>2 Spinola Road, St. Julians, STJ 3014 Malta</p>
                     <!-- <p><i class="fas fa-envelope mr-3"></i> <br/>info@esagaming.it</p> -->
                     <!-- <p><i class="fas fa-phone mr-3"></i> <br/> + 01 234 567 88</p> -->
 
